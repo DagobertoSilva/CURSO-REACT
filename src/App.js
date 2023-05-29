6280bc94e8711097378dbd8d128d5e65f4803e86
Dagoberto silva <dagobertoosilvaa2608@gmail.com>
2023-05-29 15:28:22 -0300

    Entendendo o JSX - #03

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
+ const name = 'Dagoberto'
+
+ const newname = name.toLocaleUpperCase()
+
+ function sum(a,b){
+     return a+b
+ }
+
+ const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <h1>Olá React </h1>
-      <p>Meu Primeiro App</p>
+     <h1>Alterando JSX</h1>
+     <p>olá {newname}</p>
+     <p>Soma:{sum(2,9)}</p>
+     <img src={url} alt="Minha imagem"/>
     </div>
   );
 }
