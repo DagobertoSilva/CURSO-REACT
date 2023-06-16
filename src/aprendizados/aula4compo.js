f5259d91ec584a75321f490054e1bc2a5664274e
Dagoberto silva <dagobertoosilvaa2608@gmail.com>
2023-06-16 18:17:23 -0300

    anotações da aula #4 de componentes

---
+++ src/aprendizados/aula4compo.js
@@ -0,0 +1,23 @@
+ //criando meu componente 
+function Frase() {    
+    return(
+        <div>
+            <p>
+                Este é um compenente com uma frase!
+            </p>
+        </div>
+    )
+}
+
+export default Frase
+
+// Na criação do compenente é necessário, 
+// Function nomedocomponente(){
+//     return(
+//         <div>
+//             aqui eu faço o que eu quero em meu componente,
+//         </div>
+//     )
+// }
+
+// export default Nomedocomponente  -> aqui estou exportando o componenente, permitindo ele ser usado no meu projeto em qualquer lugar.
